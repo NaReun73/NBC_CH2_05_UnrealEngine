56fde2c83cc7b63db833b9b4e1738a16bf0fcc0c
NaReun73 <rnlcksg37@gmail.com>
2026-03-24 17:21:50 +0900

    필수 기능 구현 완료

---
+++ Source/CH2_05_Unreal/MyActor.cpp
@@ -0,0 +1,128 @@
+
+#include "MyActor.h"
+#include "Engine/Engine.h"
+
+AMyActor::AMyActor()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Start"));
+	SetActorLocation(FVector(0, 50, 0));
+
+	//FVector ActorLocation = GetActorLocation();
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Red, TEXT("Cube"));
+		//GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Blue, FString::Printf(TEXT("Actor : %s"), *GetName()));
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Blue, FString::Printf(TEXT("Cube Location : %s"), *GetActorLocation().ToString()));
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Blue, FString::Printf(TEXT("Cube Rotation : %s"), *GetActorRotation().ToString()));
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		Random();
+	}
+	
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMyActor::Move(const FVector& direction)
+{
+	FVector CurrentLocation = GetActorLocation();
+	FVector NewLocation = CurrentLocation + direction;
+	SetActorLocation(NewLocation);
+	UE_LOG(LogTemp, Warning, TEXT("Vector : X = %f, Y = %f, Z = %f"), NewLocation.X, NewLocation.Y, NewLocation.Z);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Green, FString::Printf(TEXT("Move : %s"), *direction.ToString()));
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Green, FString::Printf(TEXT("Cube Location : %s"), *GetActorLocation().ToString()));
+	}
+}
+
+void AMyActor::Turn(const FRotator& rotation)
+{
+	//AddActorLocalRotation(rotation);
+
+	FRotator CurrentRotation = GetActorRotation();
+	FRotator NewRotation = CurrentRotation + rotation;
+	SetActorRotation(NewRotation);
+	UE_LOG(LogTemp, Warning, TEXT("Rotator : X = %f, Y = %f, Z = %f"), NewRotation.Pitch, NewRotation.Yaw, NewRotation.Roll);
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Orange, FString::Printf(TEXT("Turn : %s"), *rotation.ToString()));
+		GEngine->AddOnScreenDebugMessage(-1, 300.0f, FColor::Orange, FString::Printf(TEXT("Cube Rotation : %s"), *GetActorRotation().ToString()));
+	}
+}
+
+void AMyActor::Random()
+{
+	int32 RandomValue = FMath::RandRange(1, 100); // 1 ~ 100의 난수 생성
+
+	if (RandomValue < 50)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Move!"));
+		FVector RandomVector(FMath::RandRange(-50, 50), FMath::RandRange(-50, 50), FMath::RandRange(-50, 50));
+		Move(RandomVector);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Log, TEXT("Turn"));
+		FRotator RandomRotator(FMath::RandRange(-10, 10), FMath::RandRange(-10, 10), FMath::RandRange(-10, 10));
+		Turn(RandomRotator);
+	}
+}
+
+/*
+TArray<FVector> AMyActor::PrintFVectorArray()
+{
+	TArray<FVector> VectorArray;
+
+	for (int i = 0; i < 10; i++)
+	{
+		FVector RandomVector(FMath::RandRange(-50.0f, 50.0f), FMath::RandRange(-50.0f, 50.0f), FMath::RandRange(-50.0f, 50.0f));
+		//RandomVector.X = FMath::RandRange(-50.0f, 50.0f);
+		//RandomVector.Y = FMath::RandRange(-50.0f, 50.0f);
+		//RandomVector.Z = FMath::RandRange(-50.0f, 50.0f);
+		VectorArray.Add(RandomVector);
+	}
+
+	for (int32 i = 0; i < VectorArray.Num(); i++)
+	{
+		const FVector& Vec = VectorArray[i];
+		UE_LOG(LogTemp, Warning, TEXT("Vector %d: X = %f, Y = %f, Z = %f"), i, Vec.X, Vec.Y, Vec.Z);
+	}
+
+	return VectorArray;
+}
+
+TArray<FRotator> AMyActor::PrintFRotatorArray()
+{
+	TArray<FRotator> RotatorArray;
+
+	for (int i = 0; i < 10; i++)
+	{
+		FRotator RandomRotator(FMath::FRand() * 360.f, FMath::FRand() * 360.f, FMath::FRand() * 360.f);
+		//RandomRotator.Pitch = FMath::RandRange(-50.0f, 50.0f);
+		//RandomRotator.Yaw = FMath::RandRange(-50.0f, 50.0f);
+		//RandomRotator.Roll = FMath::RandRange(-50.0f, 50.0f);
+		RotatorArray.Add(RandomRotator);
+	}
+
+	for (int32 i = 0; i < RotatorArray.Num(); i++)
+	{
+		const FRotator& Rotator = RotatorArray[i];
+		UE_LOG(LogTemp, Warning, TEXT("Rotator %d: Pitch = %f, Yaw = %f, Roll = %f"), i, Rotator.Pitch, Rotator.Yaw, Rotator.Roll);
+	}
+
+	return RotatorArray;
+}*/
---
+++ Source/CH2_05_Unreal/MyActor.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+//#include "Engine/Engine.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class CH2_05_UNREAL_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float deltaTime) override;
+	void Move(const FVector& direction);
+	void Turn(const FRotator& rotation);
+	void Random();
+	//TArray<FVector> PrintFVectorArray();
+	//TArray<FRotator> PrintFRotatorArray();
+};
